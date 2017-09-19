4700ec05c81bb4341d37c469a6c25e7ab3c883f2
Roman Makarov <rk_box@mail.ru>
2017-09-19 16:28:40 +0300

    menu added, when user created it has been assigned to base role

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -9,10 +9,33 @@ use Symfony\Component\HttpFoundation\Request;
 class IndexController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="mainpage")
      */
     public function indexAction(Request $request)
     {
+
+        //Check if user authenticated
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        // replace this example code with whatever you need
+        return $this->render('index/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function usersAction(Request $request)
+    {
+
+        //Check if user authenticated
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
         // replace this example code with whatever you need
         return $this->render('index/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
---
+++ web/static/js/main.js
@@ -0,0 +1,12 @@
+function showMenuItems() {
+    var x = document.getElementById("menu");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+$( document ).ready(function(){
+
+});
