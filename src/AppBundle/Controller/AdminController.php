6875ead007192886a489b3c66259f74f46b868ce
Roman Makarov <rk_box@mail.ru>
2017-09-27 12:54:30 +0300

    reports data, calculation fields added

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -75,6 +75,30 @@ class AdminController extends Controller
             $stmt->execute();
             $deals_data = $stmt->fetchAll();
 
+            //get current seed data
+            $sql = 'SELECT s.* FROM seed_data s WHERE s.updated_at >= DATE_FORMAT(NOW(),"%Y-%m-%d 00:00:00") AND s.updated_at <= DATE_FORMAT(NOW(),"%Y-%m-%d 23:59:59") ORDER BY updated_at DESC LIMIT 1';
+            $stmt = $em->getConnection()->prepare($sql);
+            $stmt->execute();
+            $seed_data = $stmt->fetchAll();
+
+            if( !empty($seed_data) ){
+                $seed_data = $seed_data[0];
+            }
+            else{
+                //get latest seed data
+                $sql = 'SELECT s.* FROM seed_data s ORDER BY updated_at DESC LIMIT 1';
+                $stmt = $em->getConnection()->prepare($sql);
+                $stmt->execute();
+                $seed_data = $stmt->fetchAll();
+
+                if( !empty($seed_data) ){
+                    $seed_data = $seed_data[0];
+                }
+                else{
+                    return new JsonResponse(['result'=>false,'msg'=>'Unable to retrieve latest seed data.']);
+                }
+            }
+
             $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
 
             $phpExcelObject->getProperties()
@@ -101,11 +125,51 @@ class AdminController extends Controller
                 ->setCellValue('H' . $i, 'Масличность семян подсолнечника, % от АСВ')
                 ->setCellValue('I' . $i, 'Объём закупки, тонн')
                 ->setCellValue('J' . $i, 'Комментарий')
+
+                ->setCellValue('K' . $i, 'Логистика доставки семечки, руб. без НДС на тонну')
+                ->setCellValue('L' . $i, 'Цена закупки семечки на заводе (цена+логистика), руб. без НДС на тонну')
+                ->setCellValue('M' . $i, 'Цена закупки семечки на заводе (с учетом масличности и переработки), руб. без НДС на тонну')
+                ->setCellValue('N' . $i, 'Коэффициент «Альфа»')
+                ->setCellValue('O' . $i, 'Коэффициент «Омега»')
+                ->setCellValue('P' . $i, 'Минимум «Омега»')
+                ->setCellValue('Q' . $i, 'Превышение минимума «Омега»')
+                ->setCellValue('R' . $i, 'Премия на 1 тонну за превышение коэф. «Омега», руб.')
             ;
             $i++;
             foreach( $deals_data as $deal ){
 
                 $dealData = new \DateTime( $deal['updated_at'] );
+
+                $deal_logistic_price = $deal['delivery_price'] + $deal['shipment_price'] + $deal['storage_price']*3;
+                $deal_seed_purchase_price = ($deal['seed_price'] + $deal_logistic_price)*1.02;
+
+                $deal['oil_content'] = $deal['oil_content']/100;
+                if( $deal['oil_content'] > 0.48 ){
+                    $price = ($deal['oil_content'] - 0.48)*1.5*$deal['seed_price'] + $deal['seed_price'];
+                }
+                else{
+                    if( $deal['oil_content'] > 0.46 ){
+                        $price = $deal['seed_price'];
+                    }
+                    else{
+                        if( $deal['oil_content'] >= 0.43 ){
+                            $price = $deal['seed_price'] - ( 0.46 - $deal['oil_content'] )*2*$deal['seed_price'];
+                        }
+                        else{
+                            $price = $deal['seed_price'] - ( 0.06 + ( 0.43 - $deal['oil_content'] )*3 )*$deal['seed_price'];
+                        }
+                    }
+                }
+                $deal_seed_purchase_price_oil = ($price + $deal_logistic_price)*1.02 + $seed_data['processing_cost'];
+
+                $omegaNumeratorOil = (intval($seed_data['oil_price']) - 15)*intval($seed_data['usdrub']) - 2000;
+                $omegaNumeratorOilMeal = intval($seed_data['oilmeal_price'])*intval($seed_data['usdrub']) - 2000;
+
+                $alpha = ($omegaNumeratorOil + $omegaNumeratorOilMeal)/$deal_seed_purchase_price;
+
+                $oilYield        = $deal['oil_content']*100*0.91 - 1.2;
+                $oilMealYield    = 81.5 - $oilYield;
+                $omega = (($omegaNumeratorOil * $oilYield + $omegaNumeratorOilMeal * $oilMealYield) / 100) / $deal_seed_purchase_price_oil;
 
                 $phpExcelObject->setActiveSheetIndex(0)
                     ->setCellValue('A' . $i, $deal['username'])
@@ -118,6 +182,15 @@ class AdminController extends Controller
                     ->setCellValue('H' . $i, $deal['oil_content'])
                     ->setCellValue('I' . $i, $deal['purchase_volume'])
                     ->setCellValue('J' . $i, $deal['comment'])
+
+                    ->setCellValue('K' . $i, $deal_logistic_price)
+                    ->setCellValue('L' . $i, $deal_seed_purchase_price)
+                    ->setCellValue('M' . $i, $deal_seed_purchase_price_oil)
+                    ->setCellValue('N' . $i, round($alpha, 2))
+                    ->setCellValue('O' . $i, round($omega, 2))
+                    ->setCellValue('P' . $i, $seed_data['minomega'])
+                    ->setCellValue('Q' . $i, round($omega - $seed_data['minomega'], 2))
+                    ->setCellValue('R' . $i, round($omega - $seed_data['minomega'], 2)*500)
                 ;
 
                 $count--;
